53f7791c90591cf2a112ae133e2f09405a6a838f
Amit Adhikari <amitadhikari525252@gmail.com>
2024-05-12 14:50:20 +0530

    starting server setup done

--- server/server.js
+++ server/server.js
@@ -1,21 +1,29 @@
-const express = require('express');
-const cors = require('cors');
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import cors from "cors";
+
+//configure env
+dotenv.config();
 
 //rest object
 const app = express();
 
 //PORT
-const PORT = 8001;
+const PORT = process.env.PORT || 8001;
 
 //middleware
 app.use(express.json());
 app.use(cors());
 
 //rest api
-app.get("/",(req,res) => {
-  res.send("Welcome to the Shopzy")
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to the Shopzy");
+});
 
-app.listen((PORT), () => {
-  console.log(`Server is running on port: ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(
+    `Server is running ${process.env.DEV_MODE} mode on port: ${PORT}`.bgCyan
+      .white
+  );
+});
